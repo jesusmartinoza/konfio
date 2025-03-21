487299fa571f10840626d10daecbb3c2f9dcfdef
Jesus Martinez <jesusmartinoza@gmail.com>
2025-03-21 15:54:58 -0600

    Base project created

---
+++ lib/pages/dogs/dogs.page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DogsPage extends StatelessWidget {
+  const DogsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Dogs We Love')),
+    );
+  }
+}
---
+++ lib/theme/spaces.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Spaces {
+  static SizedBox vertical2 = const SizedBox(height: 2);
+  static SizedBox vertical4 = const SizedBox(height: 4);
+  static SizedBox vertical8 = const SizedBox(height: 8);
+  static SizedBox vertical12 = const SizedBox(height: 12);
+  static SizedBox vertical16 = const SizedBox(height: 16);
+  static SizedBox vertical24 = const SizedBox(height: 24);
+  static SizedBox vertical30 = const SizedBox(height: 30);
+  static SizedBox vertical32 = const SizedBox(height: 32);
+  static SizedBox vertical48 = const SizedBox(height: 48);
+
+  static SizedBox horizontal4 = const SizedBox(width: 4);
+  static SizedBox horizontal8 = const SizedBox(width: 8);
+  static SizedBox horizontal12 = const SizedBox(width: 12);
+  static SizedBox horizontal16 = const SizedBox(width: 16);
+  static SizedBox horizontal24 = const SizedBox(width: 24);
+}
